2484059c17f4803966f983fb1ed3b3ee32087285
osorio <osorio-cruz@hotmail.com>
2020-02-13 21:00:56 -0500

    Print the Json info in the logs.

--- app/src/main/java/com/example/weatherapp11/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp11/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.weatherapp11
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weatherapp11.Model.WeatherInfo
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val jsonWeatherApi: JsonWeatherApi = retrofit.create(JsonWeatherApi::class.java)
+
+        val test: Call<WeatherInfo> = jsonWeatherApi.getWeatherInfo("toronto", "65c8bbb29469fa0f101001642a325d13")
+        test.enqueue(object : Callback<WeatherInfo> {
+            override fun onFailure(call: Call<WeatherInfo>?, t: Throwable?) {
+                Log.d("TEST", "error: " + t.toString())
+            }
+
+            override fun onResponse(call: Call<WeatherInfo>?, response: Response<WeatherInfo>?) {
+                Log.d("TEST", "code: " + response?.body()?.code + "\n" +
+                        "Coordinates: " + response?.body()?.coordinates?.lat +
+                        " and " + response?.body()?.coordinates?.lon)
+            }
+        })
     }
 }
