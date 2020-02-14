a71763c21ae346b2d158b69379afee848254e5dc
osorio <osorio-cruz@hotmail.com>
2020-02-13 21:01:15 -0500

    Call the Json.

--- app/src/main/java/com/example/weatherapp11/JsonWeatherApi.kt
+++ app/src/main/java/com/example/weatherapp11/JsonWeatherApi.kt
@@ -1,4 +1,13 @@
 package com.example.weatherapp11
 
+import com.example.weatherapp11.Model.WeatherInfo
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface JsonWeatherApi {
+
+    @GET("weather")
+    fun getWeatherInfo(@Query("q") city: String, @Query("appid") id: String) : Call<WeatherInfo>
 }
