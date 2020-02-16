17e3abc7a7ffebcb43b3bb43f836d10387e8d49c
osorio <osorio-cruz@hotmail.com>
2020-02-15 21:14:55 -0500

    Remove unused library.

--- app/src/main/java/com/example/weatherapp11/JsonWeatherApi.kt
+++ app/src/main/java/com/example/weatherapp11/JsonWeatherApi.kt
@@ -3,7 +3,6 @@ package com.example.weatherapp11
 import com.example.weatherapp11.Model.WeatherInfo
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface JsonWeatherApi {
