246af483f6115b38db669b7632a9d191fd31c7b6
osorio <osorio-cruz@hotmail.com>
2020-02-18 20:21:57 -0500

    Make adapter display mutable live data.

--- app/src/main/java/com/example/weatherapp11/Adapters/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/weatherapp11/Adapters/RecyclerViewAdapter.kt
@@ -1,17 +1,25 @@
 package com.example.weatherapp11.Adapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherapp11.Model.Weather
 import com.example.weatherapp11.Model.WeatherInfo
 import com.example.weatherapp11.R
 
-class RecyclerViewAdapter(var context: Context, var weatherList: List<WeatherInfo>): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+class RecyclerViewAdapter(var context: Context) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
+    var weatherList: ArrayList<WeatherInfo?>
 
+    init {
+        weatherList = ArrayList()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.weather_item, parent, false)
@@ -19,12 +27,19 @@ class RecyclerViewAdapter(var context: Context, var weatherList: List<WeatherInf
     }
 
     override fun getItemCount(): Int {
+        Log.d("TEST", "itemcount: " + weatherList.size)
         return weatherList.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.cityName.text = "Toronto"
-        holder.feelsLike.text = weatherList[position].mainInfo.feelsLike.toString()
+        Log.d("TEST", "feels like: " + weatherList.get(position)?.mainInfo?.feelsLike)
+        holder.feelsLike.text = weatherList.get(position)?.mainInfo?.feelsLike.toString()
+    }
+
+    fun setWeather(list: ArrayList<WeatherInfo?>){
+        weatherList = list
+        Log.d("TEST", "setting weather: " + weatherList[0]?.mainInfo?.feelsLike)
     }
 
     class ViewHolder(itemHolder: View): RecyclerView.ViewHolder(itemHolder) {
