410ba61fb2bedcbfcb74321d424eae14b754cbea
osorio <osorio-cruz@hotmail.com>
2020-02-18 21:04:29 -0500

    Add activity to add items.

---
+++ app/src/main/java/com/example/weatherapp11/AddWeatherItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddWeatherItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_weather_item)
+    }
+}
