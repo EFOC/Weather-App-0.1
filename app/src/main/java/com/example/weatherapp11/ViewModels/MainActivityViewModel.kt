e31b78efe1ceafece7898c6af31a056feb2f0f40
osorio <osorio-cruz@hotmail.com>
2020-03-03 21:36:08 -0500

    Extend ApplicationViewModel to pass application.

--- app/src/main/java/com/example/weatherapp11/ViewModels/MainActivityViewModel.kt
+++ app/src/main/java/com/example/weatherapp11/ViewModels/MainActivityViewModel.kt
@@ -1,20 +1,22 @@
 package com.example.weatherapp11.ViewModels
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.example.weatherapp11.Model.WeatherInfo
 import com.example.weatherapp11.Repository
 
-class MainActivityViewModel: ViewModel() {
+class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
     var repo: Repository
-    var list: MutableLiveData<ArrayList<WeatherInfo?>>
+    var list: LiveData<ArrayList<WeatherInfo>>
 
     init {
         list = MutableLiveData()
-        repo = Repository
+        repo = Repository(application)
     }
 
-    fun getAllWeather(city: String): MutableLiveData<ArrayList<WeatherInfo?>> {
+    fun getAllWeather(city: String): LiveData<ArrayList<WeatherInfo>> {
         list = repo.getWeather(city)
         return list
     }
