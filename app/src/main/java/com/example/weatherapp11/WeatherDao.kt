193b3c946ccef9564173f784faa3279f219ae973
osorio <osorio-cruz@hotmail.com>
2020-03-03 21:34:37 -0500

    Create database functions.

--- app/src/main/java/com/example/weatherapp11/WeatherDao.kt
+++ app/src/main/java/com/example/weatherapp11/WeatherDao.kt
@@ -1,7 +1,9 @@
 package com.example.weatherapp11
 
+import androidx.lifecycle.LiveData
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
 import com.example.weatherapp11.Model.WeatherInfo
 
@@ -17,4 +19,6 @@ interface WeatherDao {
     fun delete(weather: WeatherInfo)
 
 
+    @Query("SELECT * FROM weather_table")
+    fun getAllWeather(): LiveData<ArrayList<WeatherInfo>>
 }
