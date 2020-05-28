995d9543679bbbde26c0026584c30a7bd1f888ca
osorio <osorio-cruz@hotmail.com>
2020-05-28 14:55:23 -0400

    Create insert function to add new weather objects into the database. Create function to retrieve objects from the database and change the object from WeatherEntity to WeatherInfo object.

--- app/src/main/java/com/example/weatherapp11/ViewModels/MainActivityViewModel.kt
+++ app/src/main/java/com/example/weatherapp11/ViewModels/MainActivityViewModel.kt
@@ -1,23 +1,57 @@
 package com.example.weatherapp11.ViewModels
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.*
+import com.example.weatherapp11.Model.MainWeatherInfo
 import com.example.weatherapp11.Model.WeatherInfo
 import com.example.weatherapp11.Repository
+import com.example.weatherapp11.WeatherDatabase
+import com.example.weatherapp11.WeatherEntity
+import kotlinx.coroutines.launch
 
 class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
-    var repo: Repository
-    var list: LiveData<ArrayList<WeatherInfo>>
+    private var repo: Repository
+    var weatherList: ArrayList<LiveData<WeatherInfo>>
 
     init {
-        list = MutableLiveData()
-        repo = Repository(application)
+        val weatherDao = WeatherDatabase.getInstance(application, viewModelScope)!!.weatherDao()
+        repo = Repository(weatherDao)
+        weatherList = ArrayList()
     }
 
-    fun getAllWeather(city: String): LiveData<ArrayList<WeatherInfo>> {
-        list = repo.getWeather(city)
-        return list
+    fun getWeather(city: String): LiveData<WeatherInfo> {
+        return repo.getWeather(city)
+    }
+
+    fun getAll(): LiveData<ArrayList<WeatherInfo>> {
+        val repoList: LiveData<List<WeatherEntity>> = repo.getAll()
+        val liveList: LiveData<ArrayList<WeatherInfo>>
+        liveList = Transformations.map(repoList) { list ->
+            val newList: ArrayList<WeatherInfo> = ArrayList()
+            list?.forEach {
+                val temp = WeatherInfo()
+                temp.mainInfo = MainWeatherInfo()
+                temp.nameOfCity = it.nameOfCity
+                temp.mainInfo.feelsLike = it.feelsLike
+                temp.mainInfo.maxTemp = it.maxTemp
+                temp.mainInfo.minTemp = it.minTemp
+                newList.add(temp)
+            }
+            return@map newList
+        }
+        return liveList
+    }
+
+    // @TODO Insert only name of city
+    fun insert(weatherInfo: WeatherInfo) {
+        viewModelScope.launch {
+            val weatherEntity = WeatherEntity(
+                weatherInfo.nameOfCity,
+                weatherInfo.mainInfo.feelsLike,
+                weatherInfo.mainInfo.maxTemp,
+                weatherInfo.mainInfo.minTemp
+            )
+            repo.insert(weatherEntity)
+        }
     }
 }
