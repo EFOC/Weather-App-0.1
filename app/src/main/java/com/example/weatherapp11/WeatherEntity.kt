b425174444dec3d963aa2dac722b08d6e2a08c45
osorio <osorio-cruz@hotmail.com>
2020-05-28 15:01:45 -0400

    Comment out variables.

--- app/src/main/java/com/example/weatherapp11/WeatherEntity.kt
+++ app/src/main/java/com/example/weatherapp11/WeatherEntity.kt
@@ -2,19 +2,30 @@ package com.example.weatherapp11
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.weatherapp11.Model.Coordinates
+import com.example.weatherapp11.Model.MainWeatherInfo
+import com.example.weatherapp11.Model.Weather
 
 @Entity(tableName = "weather_table")
-class WeatherEntity {
+class WeatherEntity(var nameOfCity: String,
+var feelsLike: Float,
+var maxTemp: Float,
+var minTemp: Float) {
 
     @PrimaryKey(autoGenerate = true)
     lateinit var id: Integer
 
-    lateinit var nameOfCity: String
+//    lateinit var nameOfCity: String
 
-    var feelsLike: Float = 0F
+//    var feelsLike: Float = 99F
 
-    var maxTemp: Float = 0F
+//    var maxTemp: Float = 99F
+//
+//    var minTemp: Float = 99F
 
-    var minTemp: Float = 0F
-
+//    var coordinates: Coordinates = Coordinates()
+//
+//    var mainInfo: MainWeatherInfo = MainWeatherInfo()
+//
+//    var weather: List<Weather> = List<Weather>
 }
