52dcc6a8dc84cce03a6b0f3b78d517b7279ed366
osorio <osorio-cruz@hotmail.com>
2020-05-28 14:58:50 -0400

    Create getAll function to get all the objects from database. Create insert function to insert Weather objects into database.

--- app/src/main/java/com/example/weatherapp11/Repository.kt
+++ app/src/main/java/com/example/weatherapp11/Repository.kt
@@ -1,9 +1,9 @@
 package com.example.weatherapp11
 
-import android.app.Application
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.weatherapp11.Model.Weather
 import com.example.weatherapp11.Model.WeatherInfo
 import retrofit2.Call
 import retrofit2.Callback
@@ -11,67 +11,41 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class Repository(application: Application) {
+class Repository(var weatherDao: WeatherDao) {
 
-    lateinit var jsonWeatherApi: JsonWeatherApi
+    private var jsonWeatherApi: JsonWeatherApi
     lateinit var cities: Call<WeatherInfo>
-    var list: LiveData<ArrayList<WeatherInfo>> = MutableLiveData()
-    var allWeatherInfo: LiveData<ArrayList<WeatherInfo>> = TODO()
-    lateinit var weatherDoa: WeatherDao
 
     init {
-        Log.d("TEST", "Creating repo")
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.openweathermap.org/data/2.5/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         jsonWeatherApi = retrofit.create(JsonWeatherApi::class.java)
-        val weatherDatabase = WeatherDatabase.getInstance(application)
-        if (weatherDatabase != null) {
-            weatherDoa = weatherDatabase.weatherDao()
-            allWeatherInfo = weatherDoa.getAllWeather()
-        }
     }
 
-    fun getWeather(city: String): LiveData<ArrayList<WeatherInfo>> {
-        var weather: WeatherInfo? = null
+    fun getWeather(city: String): LiveData<WeatherInfo> {
         cities = jsonWeatherApi.getWeatherInfo(city, "65c8bbb29469fa0f101001642a325d13")
+        val liveWeather: MutableLiveData<WeatherInfo> = MutableLiveData()
         cities.enqueue(object : Callback<WeatherInfo> {
-
             override fun onFailure(call: Call<WeatherInfo>?, t: Throwable?) {
                 Log.d("TEST", "Code: " + t.toString())
             }
 
             override fun onResponse(call: Call<WeatherInfo>?, response: Response<WeatherInfo>?) {
                 if (response!!.isSuccessful) {
-                    weather = response.body()
-                    allWeatherInfo.value?.add(weather!!)
-                    Log.d("TEST", "Repo, Feels like: " + response.body()?.mainInfo?.feelsLike)
-                    list = allWeatherInfo
+                    liveWeather.value = response.body()
                 }
             }
         })
-        Log.d("TEST", "repo: " + weather?.mainInfo?.feelsLike)
-        return list
+        return liveWeather
     }
 
-    fun insert(weatherInfo: WeatherInfo) {
-
+    suspend fun insert(weatherEntity: WeatherEntity) {
+        weatherDao.insert(weatherEntity)
     }
 
-    fun delete(weatherInfo: WeatherInfo) {
-
-    }
-
-    fun update(weatherInfo: WeatherInfo) {
-
-    }
-
-    fun deleteAllInfo() {
-
-    }
-
-    fun getAllInfo(): LiveData<ArrayList<WeatherInfo>> {
-        return allWeatherInfo
+    fun getAll(): LiveData<List<WeatherEntity>> {
+        return weatherDao.getAllWeather()
     }
 }
