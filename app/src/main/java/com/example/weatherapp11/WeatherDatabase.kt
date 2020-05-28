573e18b4d291a56bacf1470c8ef2e7d267618191
osorio <osorio-cruz@hotmail.com>
2020-05-28 15:01:01 -0400

    Create database. Create function to populate database.

--- app/src/main/java/com/example/weatherapp11/WeatherDatabase.kt
+++ app/src/main/java/com/example/weatherapp11/WeatherDatabase.kt
@@ -4,25 +4,48 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 
-@Database(entities = [WeatherEntity::class], version = 1)
+@Database(entities = [WeatherEntity::class], version = 1, exportSchema = false)
 abstract class WeatherDatabase: RoomDatabase() {
     abstract fun weatherDao(): WeatherDao
 
     companion object {
-       private var instance: WeatherDatabase? = null
+        @Volatile
+        private var INSTANCE: WeatherDatabase? = null
+
         @Synchronized
-        fun getInstance(context: Context): WeatherDatabase? {
-            if (instance != null) {
-                instance = Room.databaseBuilder(
+        fun getInstance(context: Context, scope: CoroutineScope): WeatherDatabase? {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(
                     context.applicationContext,
                     WeatherDatabase::class.java,
                     "weather_database")
+                    .addCallback(WeatherDatabaseCallback(scope))
                     .fallbackToDestructiveMigration()
                     .build()
             }
-            return instance
+            return INSTANCE
+        }
+    }
+
+    private class WeatherDatabaseCallback(private val scope: CoroutineScope) : RoomDatabase.Callback() {
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { weatherDatabase ->
+                scope.launch {
+                    populateDatabase(weatherDatabase.weatherDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(weatherDao: WeatherDao) {
+            weatherDao.deleteAll()
+            weatherDao.insert(WeatherEntity("TEST1", 20F, 2F, 3F))
+            weatherDao.insert(WeatherEntity("TEST2", 40F, 2F, 3F))
         }
     }
 }
