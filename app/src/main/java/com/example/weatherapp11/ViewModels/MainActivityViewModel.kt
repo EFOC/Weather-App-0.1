64212a80b2f8fccef82597c611fdb18a0e43638f
osorio <osorio-cruz@hotmail.com>
2020-06-02 17:49:47 -0400

    Delete unused variables. Change return type on getWeather function. Change arg type on getWeather function. Change getAll function to runBlocking. Change return arg in getAll function.

--- app/src/main/java/com/example/weatherapp11/ViewModels/MainActivityViewModel.kt
+++ app/src/main/java/com/example/weatherapp11/ViewModels/MainActivityViewModel.kt
@@ -1,17 +1,21 @@
 package com.example.weatherapp11.ViewModels
 
 import android.app.Application
-import androidx.lifecycle.*
-import com.example.weatherapp11.Model.MainWeatherInfo
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.weatherapp11.Model.WeatherInfo
 import com.example.weatherapp11.Repository
 import com.example.weatherapp11.WeatherDatabase
-import com.example.weatherapp11.WeatherEntity
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 
 class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
     private var repo: Repository
     var weatherList: ArrayList<LiveData<WeatherInfo>>
+    var mediatorLiveData = MediatorLiveData<List<Any>>()
 
     init {
         val weatherDao = WeatherDatabase.getInstance(application, viewModelScope)!!.weatherDao()
@@ -19,12 +23,12 @@ class MainActivityViewModel(application: Application) : AndroidViewModel(applica
         weatherList = ArrayList()
     }
 
-    fun getWeather(city: String): LiveData<WeatherInfo> {
-        return repo.getWeather(city)
+    fun getWeather(cities: List<String>): LiveData<ArrayList<WeatherInfo>> {
+        return repo.getWeather(cities)
     }
 
-    fun getAll(): LiveData<List<String>> {
-        return repo.getAll()
+    fun getAll(): LiveData<ArrayList<WeatherInfo>> = runBlocking {
+        return@runBlocking getWeather(repo.getAll())
     }
 
     // @TODO Insert only name of city
