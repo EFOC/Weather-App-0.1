bf3c78355410ca37824abba3afdc186240bacf18
osorio <osorio-cruz@hotmail.com>
2020-06-05 14:25:05 -0400

    Remove unused imports. Make variables private.

--- app/src/main/java/com/example/weatherapp11/AddWeatherItemActivity.kt
+++ app/src/main/java/com/example/weatherapp11/AddWeatherItemActivity.kt
@@ -1,30 +1,24 @@
 package com.example.weatherapp11
 
-import android.R.attr.apiKey
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.gms.common.api.Status
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
-import com.google.android.libraries.places.api.net.PlacesClient
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
-import java.util.*
 
 
 class AddWeatherItemActivity : AppCompatActivity() {
 
     private val API_KEY = "AIzaSyCI9kZvQL35PVVrVYtrNJChXS9itiH1k9s"
-    lateinit var completeButton: Button
-    lateinit var addWeatherItemViewModel: AddWeatherItemViewModel
+    private lateinit var completeButton: Button
+    private lateinit var addWeatherItemViewModel: AddWeatherItemViewModel
     private var placeSelected: String = String()
 
     override fun onCreate(savedInstanceState: Bundle?) {
